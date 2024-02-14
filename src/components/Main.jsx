e234517fc35171a6e04e6a37d6fa103311af06dc
YAcodingroom <n12a18a@gmail.com>
2024-02-14 12:42:00 +0800

    refactor: concat Register component to Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import Register from './stepProgress/Register'
 import Cart from './stepProgress/Cart'
 import ProgressControl from './stepProgress/ProgressControl'
+import StepProgressBar from './stepProgress/StepProgressBar'
+import Form from './stepProgress/StepProgressBar'
+
+function Register({ children }) {
+	return (
+		<section
+			className="register-container col col-lg-6 col-sm-12"
+			data-phase="1"
+			data-total-price="0"
+		>
+			<h2 className="register-title col col-12">結帳</h2>
+			{children}
+		</section>
+	)
+}
 
 export default function Main() {
 	return (
 		<main className="site-main">
 			<div className="main-container">
-				<Register />
+				<Register>
+					<StepProgressBar />
+					<Form />
+				</Register>
 				<Cart />
 				<ProgressControl />
 			</div>
